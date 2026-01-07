7e89551e8b01f26555daebd45bf3abffdcdb7c3f
Dev Kaushik <125669588+DevKaushik0007@users.noreply.github.com>
2026-01-07 18:47:47 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,25 +1,22 @@
-import axios from "axios"; 
+import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
 const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
-const headers ={
-    Authorization: "bearer " + TMDB_TOKEN,
+
+const headers = {
+    Authorization: "Bearer " + TMDB_TOKEN,   // MUST be capital Bearer
+    "Content-Type": "application/json",
 };
 
-// async function test(){
-//     const res =  await axios.get(`${BASE_URL}/https://api.themoviedb.org/3`)
-//     console.log(res.data)
-//  }
-export const fetchDataFromApi =async(url, params) => {
-    try{
-        const {data} = await axios.get(BASE_URL + url,{
-          headers,
-          params,  
+export const fetchDataFromApi = async (url, params) => {
+    try {
+        const { data } = await axios.get(BASE_URL + url, {
+            headers,
+            params,
         });
-
         return data;
-    } catch (err){
-        console.log(err);
-        return err;
-    };
+    } catch (err) {
+        console.error("TMDB API Error:", err.response?.data || err.message);
+        throw err; // important
+    }
 };
