9925f88e8a6f1841d3e9147d8b419156e174338d
DevKaushik0007 <kaushikdev786786@gmail.com>
2026-03-25 19:45:37 +0530

    fixed api.js merge conflict

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,32 +4,24 @@ const BASE_URL = "https://api.themoviedb.org/3";
 const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
 const headers = {
-    Authorization: "Bearer " + TMDB_TOKEN,   // MUST be capital Bearer
+    Authorization: "Bearer " + TMDB_TOKEN,
     "Content-Type": "application/json",
 };
 
 export const fetchDataFromApi = async (url, params) => {
     try {
         const { data } = await axios.get(BASE_URL + url.trim(), {
-   headers,
-   params: {
-      language: "en-US",
-      page: 1,
-      ...params,
-   },
-});
+            headers,
+            params: {
+                language: "en-US",
+                page: 1,
+                ...params,
+            },
+        });
 
         return data;
-<<<<<<< HEAD
     } catch (err) {
         console.error("TMDB API Error:", err.response?.data || err.message);
-        throw err; // important
+        throw err;
     }
 };
-=======
-    } catch (err){
-        console.log(err);
-        return err;
-    }
-};
->>>>>>> eaa593e (updated backend proxy and fixed TMDB API issue)
